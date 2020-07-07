39d1aa278167654d7d3acff9fb0e39d7e35ca849
jideabdqudus <jideabdqudus@gmail.com>
2020-07-07 15:51:45 +0100

    Added Commit

---
+++ Trash.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Trash = () => {
+  return (
+    <div>
+      <div>Client ID: _JLBMCS7sHkSW0qENlhvYg</div>
+      <div>
+        API Key:
+        FYii8Ju7OZbl1R9Q250-qwHMIGOp4D3Xdzqy3Hsv9wCc7qCkoP33H4fP8PMTrMIa-r7ZOOnxaW-SRNhx8ru5quNlyGczOiCrXEvuGX1761O-iILUW9lOVDFrj4sEX3Yx
+      </div>
+    </div>
+  );
+};
+
+export default Trash;
