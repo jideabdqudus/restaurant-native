f8c644a974108a4dc27e63ecad25c1c083deee7a
jideabdqudus <jideabdqudus@gmail.com>
2020-07-09 15:10:00 +0100

    Added Commit

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,39 +1,38 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [error, setError] = useState("");
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: "san jose",
-      },
-    });
-    setResults(response.data.businesses);
-    console.log(results.length);
+    console.log("Hi there!");
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setError("Someting went Wrong here");
+    }
   };
 
-  const onTermChange = (newTerm) => {
-    setTerm(newTerm);
-  };
-
-  const onTermSubmit = () => {
-    console.log(`Submitted`);
-    setTerm("");
-  };
+  useEffect(() => {
+    searchApi("Pasta");
+  }, []);
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} searchApi={searchApi} />
-      <Text>
-        {results.length}
-      </Text>
+      <Text>{results.length}</Text>
+      <Text>{error}</Text>
     </View>
   );
 };
