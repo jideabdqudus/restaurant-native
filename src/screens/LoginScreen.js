5c7ab23bac3af670495fe43de68d45f8f1f15e41
jideabdqudus <jideabdqudus@gmail.com>
2020-08-02 23:14:27 +0100

    Added Commit

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -7,7 +7,9 @@ const LoginScreen = () => {
     <View>
       <Text style={styles.first}>Welcome to</Text>
       <Text style={styles.second}>Restour</Text>
-      <Text style={styles.second}>Restour</Text>
+      <Text style={styles.third}>
+        Enter the following details
+      </Text>
     </View>
   );
 };
@@ -17,13 +19,24 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginLeft: 20,
     marginTop: 20,
-    letterSpacing: 2
+    letterSpacing: 2,
   },
   second: {
     fontSize: 30,
     marginLeft: 20,
     letterSpacing: 2,
-    fontWeight: "bold"
+    fontWeight: "bold",
+  },
+  third: {
+    fontSize: 15,
+    marginTop: 10,
+    marginLeft: 20,
+    backgroundColor: "#fed304",
+    borderRadius:15,
+    position: "absolute",
+    width: 222,
+    height: 85,
+    left: 90
   },
 });
 
