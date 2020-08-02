ccc4764ddca9c99f5e068ee19fe39f59d4a1eb02
jideabdqudus <jideabdqudus@gmail.com>
2020-08-02 23:07:54 +0100

    Added Commit

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import SearchScreen from './src/screens/SearchScreen';
-import ResultsShow from './src/screens/ResultsShow';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./src/screens/SearchScreen";
+import ResultsShow from "./src/screens/ResultsShow";
+import LoginScreen from "./src/screens/LoginScreen";
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
     Result: ResultsShow,
+    Login: LoginScreen,
   },
   {
-    initialRouteName: 'Search',
-    defaultNavigationOptions:{
-      title: "Business Search"
-    }
+    initialRouteName: "Login",
+    defaultNavigationOptions: {
+      title: "Business Search",
+    },
   }
-)
+);
 
-
-export default createAppContainer(navigator)
+export default createAppContainer(navigator);
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import {View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
 
 const LoginScreen = () => {
-    return (
-        <>
-          <View>
-          
-          </View>  
-        </>
-    )
-}
+  return (
+    <View>
+      <Text style={styles.first}>Welcome to</Text>
+      <Text style={styles.second}>Restour</Text>
+      <Text style={styles.second}>Restour</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
+  first: {
+    fontSize: 20,
+    marginLeft: 20,
+    marginTop: 20,
+    letterSpacing: 2
+  },
+  second: {
+    fontSize: 30,
+    marginLeft: 20,
+    letterSpacing: 2,
+    fontWeight: "bold"
+  },
+});
 
-})
-
-export default LoginScreen
+export default LoginScreen;
