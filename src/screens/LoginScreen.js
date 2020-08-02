f40108b92185110dacf9bca4a66f7ab241bb18da
jideabdqudus <jideabdqudus@gmail.com>
2020-08-02 22:59:28 +0100

    Added Commit

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {View, Text, StyleSheet} from 'react-native'
+
+const LoginScreen = () => {
+    return (
+        <>
+          <View>
+          
+          </View>  
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default LoginScreen
